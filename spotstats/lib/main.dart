24d3f4497fb6f7127de7dd7b16a0060161f726f0
lksmasin <mail@lukymas.eu.org>
2024-12-14 15:03:16 +0100

    Updates

--- spotstats/lib/main.dart
+++ spotstats/lib/main.dart
@@ -1,19 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  await dotenv.load(fileName: "./.env");
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spotify Stats',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _storage = const FlutterSecureStorage();
+
+  Future<void> _authenticate() async {
+    final clientId = dotenv.env['SPOTIFY_CLIENT_ID'];
+    final redirectUri = dotenv.env['SPOTIFY_REDIRECT_URI'];
+    final scopes = 'user-top-read';
+
+    final authUrl =
+        'https://accounts.spotify.com/authorize?response_type=code&client_id=$clientId&redirect_uri=$redirectUri&scope=$scopes';
+
+    try {
+      final result = await FlutterWebAuth.authenticate(
+        url: authUrl,
+        callbackUrlScheme: Uri.parse(redirectUri!).scheme,
+      );
+
+      final code = Uri.parse(result).queryParameters['code'];
+      if (code != null) {
+        await _getAccessToken(code);
+      }
+    } catch (e) {
+      print('Authentication error: $e');
+    }
+  }
+
+  Future<void> _getAccessToken(String code) async {
+    final clientId = dotenv.env['SPOTIFY_CLIENT_ID'];
+    final clientSecret = dotenv.env['SPOTIFY_CLIENT_SECRET'];
+    final redirectUri = dotenv.env['SPOTIFY_REDIRECT_URI'];
+
+    final response = await http.post(
+      Uri.parse('https://accounts.spotify.com/api/token'),
+      headers: {
+        'Authorization':
+            'Basic ${base64Encode(utf8.encode('$clientId:$clientSecret'))}',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': redirectUri,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      await _storage.write(key: 'access_token', value: data['access_token']);
+      await _storage.write(key: 'refresh_token', value: data['refresh_token']);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => StatsPage()),
+      );
+    } else {
+      print('Failed to get access token: ${response.body}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Spotify Login'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _authenticate,
+          child: Text('Login with Spotify'),
         ),
+      ),
+    );
+  }
+}
+
+class StatsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Spotify Stats'),
+      ),
+      body: Center(
+        child: Text('Welcome to Spotify Stats!'),
       ),
     );
   }
