b8d8e021eeb9565450c88b2233f6c6ee820155d5
lksmasin <lksmasin@gmail.com>
2025-01-03 13:44:35 +0100

    edited env mechanism

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,16 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ujistěte se, že se widgety správně inicializují
-  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    // Pokus o načtení .env souboru
+    await dotenv.load(fileName: ".env");
+    print('Loaded .env file.');
+  } catch (e) {
+    print('Failed to load .env file. Using fallback to dart-define.');
+  }
+
   runApp(MyApp());
 }
 
